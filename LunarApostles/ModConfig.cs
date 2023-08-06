1c4613c2a6e3e752c9f0d067b14361f20720e295
FF <focused.fault@gmail.com>
2023-08-06 10:48:09 -0400

    config fix

--- LunarApostles/ModConfig.cs
+++ LunarApostles/ModConfig.cs
@@ -16,7 +16,7 @@ namespace LunarApostles
     {
       enableAdaptiveArmor = config.Bind("General", "Adaptive Armor", true, "Enable Adaptive Armor on the scavs (same armor mithrix has)");
       sBaseHealth = config.Bind("Stats", "Base HP", 500f, "Vanilla: 3k-4k depending on scav (they also didnt have adaptive armor)");
-      sLevelHealth = config.Bind("Stats", " Level HP", 150f, "Health gained per level. Vanilla: 1k-1.2k depending on scav");
+      sLevelHealth = config.Bind("Stats", "Level HP", 150f, "Health gained per level. Vanilla: 1k-1.2k depending on scav");
       baseDamage = config.Bind("Stats", "Base Damage", 8f, "Vanilla: 4");
       levelDamage = config.Bind("Stats", "Level Damage", 1.6f, "Damage gained per level. Vanilla: 0.8");
       oBaseHealth = config.Bind("Other Stats", "Tracking Orb Base Health", 180f, "Vanilla: 75");
